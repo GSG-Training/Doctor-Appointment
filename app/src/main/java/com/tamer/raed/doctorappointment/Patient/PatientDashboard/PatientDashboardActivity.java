7a5b31193b33b62cc54a2508886725683da55e12
Tamer Raed <tameralna99@gmail.com>
2020-12-12 19:54:40 +0200

    Create patient search activity

--- app/src/main/java/com/tamer/raed/doctorappointment/Patient/PatientDashboard/PatientDashboardActivity.java
+++ app/src/main/java/com/tamer/raed/doctorappointment/Patient/PatientDashboard/PatientDashboardActivity.java
@@ -1,8 +1,10 @@
 package com.tamer.raed.doctorappointment.Patient.PatientDashboard;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -84,5 +86,19 @@ public class PatientDashboardActivity extends AppCompatActivity implements Navig
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction().setCustomAnimations(android.R.animator.fade_in,
                 android.R.animator.fade_out).replace(R.id.patient_frameLayout, fragment).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu_icon, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_search_item) {
+            startActivity(new Intent(PatientDashboardActivity.this, SearchActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/tamer/raed/doctorappointment/Patient/PatientDashboard/SearchActivity.java
@@ -0,0 +1,35 @@
+package com.tamer.raed.doctorappointment.Patient.PatientDashboard;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.tamer.raed.doctorappointment.R;
+
+public class SearchActivity extends AppCompatActivity {
+    SearchView searchView;
+    RecyclerView rv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//  set status text dark
+        }
+        getWindow().setStatusBarColor(ContextCompat.getColor(SearchActivity.this, R.color.white));// set status background white
+
+        searchView = findViewById(R.id.patient_searchView);
+        rv = findViewById(R.id.patient_search_rv);
+    }
+
+    public void back(View view) {
+        onBackPressed();
+    }
+}
