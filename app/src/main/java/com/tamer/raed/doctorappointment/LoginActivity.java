ac8ebf73999320d456478f4af5a4ad03a1b42f98
Al-Hasan A. Al-Khatib <47783147+A-lHasan-AlKhatib@users.noreply.github.com>
2021-02-05 19:24:30 +0200

    Fix 1

--- app/src/main/java/com/tamer/raed/doctorappointment/LoginActivity.java
+++ app/src/main/java/com/tamer/raed/doctorappointment/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,7 +19,6 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         initViews();
-        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
 
         tvSignUp.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this, WelcomeActivity.class)));
         login_btn.setOnClickListener(view -> {
